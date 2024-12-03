8df6316db4f05b1e040edd6aa5811815fd3fe771
CoderBin <1669940775@qq.com>
2024-12-03 22:28:27 +0800

    feat: 使用react和tailwind插件

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,27 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-// import vercel from '@astrojs/vercel/serverless';
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      title: 'Joey',
-      social: {
-        github: 'https://github.com/zjunbin1286',
+  integrations: [starlight({
+    title: 'Joey',
+    social: {
+      github: 'https://github.com/zjunbin1286',
+    },
+    sidebar: [
+      {
+        label: 'Guides',
+        items: [
+          // Each item here is one entry in the navigation menu.
+          { label: 'Example Guide', slug: 'guides/example' },
+        ],
       },
-      sidebar: [
-        {
-          label: 'Guides',
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: 'Example Guide', slug: 'guides/example' },
-          ],
-        },
-        {
-          label: 'Reference',
-          autogenerate: { directory: 'reference' },
-        },
-      ],
-    }),
-  ],
-  // output: 'server',
-  // adapter: vercel(),
-  // site: 'https://zjunbin1286.github.io',
-  // base: 'joey-blog',
+      {
+        label: 'Reference',
+        autogenerate: { directory: 'reference' },
+      },
+    ],
+  }), react(), tailwind()],
 });
