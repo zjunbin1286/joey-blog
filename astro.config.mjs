e1e8863753175967c64f0b851d941280bcad3e73
CoderBin <1669940775@qq.com>
2024-12-16 22:09:54 +0800

    feat: 前端必知必会专栏增加《说说 var、let、const 三者的区别》

--- astro.config.mjs
+++ astro.config.mjs
@@ -84,6 +84,7 @@ export default defineConfig({
           { label: '说说 Vue 中的组件和插件有什么区别？', slug: 'articles/interview/01-component-plugin' },
           { label: '说说你对 Set、Map 的理解', slug: 'articles/interview/02-set-map' },
           { label: '谈谈你知道的 DOM 的常见操作', slug: 'articles/interview/03-dom-operate' },
+          { label: '说说 var、let、const 三者的区别', slug: 'articles/interview/04-var-let-const' },
         ]
       }
       // {
