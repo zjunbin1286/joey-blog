1981913ba1178d14f813f3690655b1ea74992076
CoderBin <1669940775@qq.com>
2025-01-09 22:14:34 +0800

    feat: 作品集增加Docs Unity

--- src/pages/components/_Collections/data.ts
+++ src/pages/components/_Collections/data.ts
@@ -7,6 +7,11 @@ interface ListItem {
 
 export const listData: ListItem[] = [
   {
+    title: 'Docs Unity',
+    url: 'https://docs-unity.online/',
+    description: '在线多人协同文档，该 Docs 包含了现代文档编辑器所具备的功能，包括但不限于表格插入，字体定制，图片上传等等。支持 JSON、PDF、HTML、Text 等格式导出方式。使用 GitHub、Google 快捷登录。该 Docs 同时支持个人和组织账户，全面有效的邀请系统，和与组织内成员共同管理及编辑文档。可以对一个文档发起讨论，建立了一个通知系统，可以跟踪左右提及和评论，随时@组织成员分享文档内容。提供实时同步、富文本的编辑和用户管理。技术实现：Next.js15、React19、Tiptap、Shadcn/ui、Nuqs、Convex、LiveBlocks、Clerk'
+  },
+  {
     title: 'npm-registry-nrs',
     url: 'collections/npm-registry-nrs',
     description: '📦 npm-registry-nrs is an NPM source manager that allows you to quickly switch between NPM sources and supports managing image sources through addition, deletion, modification, and querying. （npm-registry-nrs 是一个 npm 源管理器，允许你快速地在 npm 源间切换，并且支持通过增删改查等管理镜像源。）'
