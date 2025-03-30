93dfa0369f4d523824f5560bfeaee2083b47bbaf
CoderBin <1669940775@qq.com>
2025-03-30 15:12:17 +0800

    style: 优化问题记录和社区转发的页面样式

--- src/pages/components/_CommunityArticle/index.tsx
+++ src/pages/components/_CommunityArticle/index.tsx
@@ -12,7 +12,7 @@ export default function CommunityArticle() {
       {listData.map((item, index) => (
         <div key={item.url} className={styles.item}>
           <div className={styles.title}>
-            <a href={item.url}>{item.title}</a>
+            <a className='text-[20px]' href={item.url}>{item.title}</a>
           </div>
           <div className={styles.bottom}>
             <span className={styles.time}>时间：{item.createDate}</span>
