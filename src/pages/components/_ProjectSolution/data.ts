8bce750c78603f95e982a2230e350628d0911948
CoderBin <1669940775@qq.com>
2025-03-30 13:22:44 +0800

    feat: 在项目中遇到问题以及如何解决的方案栏目增加《使用深度优先搜索查找父子级关联的数据》

--- src/pages/components/_ProjectSolution/data.ts
+++ src/pages/components/_ProjectSolution/data.ts
@@ -8,6 +8,11 @@ export const listData: ListItem[] = [
   {
     url: '/project-solution/01-resize-observer',
     title: '浏览器内置API，ResizeObserver 的使用，用于监听DOM元素尺寸的变化，可以在回调内处理对应逻辑',
-    createDate: '2024-12-05',
+    createDate: '2024-03-25',
+  },
+  {
+    url: '/project-solution/02-depth-first-search-parent-children',
+    title: '使用深度优先搜索查找父子级关联的数据',
+    createDate: '2024-03-30',
   },
 ]
