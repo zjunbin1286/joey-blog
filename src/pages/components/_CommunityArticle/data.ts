3de1cece3991e4ea572ea975dee8d2e0a22be1be
CoderBin <1669940775@qq.com>
2025-03-30 14:57:20 +0800

    feat: 增加"社区转发"栏目，新增《前端定时任务的神库 Cron，快把它加到你的项目中去！》文章

---
+++ src/pages/components/_CommunityArticle/data.ts
@@ -0,0 +1,15 @@
+interface ListItem {
+  title: string;
+  url: string;
+  from: string;
+  createDate: string;
+}
+
+export const listData: ListItem[] = [
+  {
+    url: '/community-articles/01-cron',
+    title: '前端定时任务的神库 Cron，快把它加到你的项目中去！',
+    createDate: '2024-03-30',
+    from: '微信公众号'
+  },
+]
--- src/pages/components/_CommunityArticle/index.tsx
+++ src/pages/components/_CommunityArticle/index.tsx
@@ -3,19 +3,20 @@ import { listData } from './data'
 import styles from './styles.module.scss';
 
 /**
- * 工作中的问题记录
+ * 社区转发
  * @returns 
  */
-export default function ProjectSolution() {
+export default function CommunityArticle() {
   return (
-    <div className={styles.ProjectSolutionContent}>
+    <div className={styles.CommunityArticleContent}>
       {listData.map((item, index) => (
         <div key={item.url} className={styles.item}>
           <div className={styles.title}>
             <a href={item.url}>{item.title}</a>
           </div>
           <div className={styles.bottom}>
-            <span className={styles.time}>{item.createDate}</span>
+            <span className={styles.time}>时间：{item.createDate}</span>
+            <span className={styles.from}>来源：{item.from}</span>
           </div>
         </div>
       ))}
