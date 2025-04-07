5acdbb1386333a284698365061e3099a313a2d58
CoderBin <1669940775@qq.com>
2025-04-07 20:36:44 +0800

    feat: 社区转发新增《前端神库 Puppeteer ！让你能在 Nodejs 里操作浏览器玩出花样！》

--- src/pages/components/_CommunityArticle/data.ts
+++ src/pages/components/_CommunityArticle/data.ts
@@ -8,8 +8,14 @@ interface ListItem {
 export const listData: ListItem[] = [
   {
     url: 'https://mp.weixin.qq.com/s/x75dqt6FDHk5OEv13tr_tw',
-    title: '前端定时任务的神库 Cron，快把它加到你的项目中去！',
+    title: '前端定时任务的神库 Cron！，快把它加到你的项目中去！',
     createDate: '2024-03-30',
+    from: '微信公众号'
+  },
+  {
+    url: 'https://mp.weixin.qq.com/s/24RhBwvDyS4GVWhwWVHgkQ',
+    title: '前端神库 Puppeteer ！让你能在 Nodejs 里操作浏览器玩出花样！',
+    createDate: '2024-04-07',
     from: '微信公众号'
   },
 ]
