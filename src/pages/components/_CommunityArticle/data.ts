9c2bedad09cc43c03f4853d79bdf5a04b5b314fd
CoderBin <1669940775@qq.com>
2025-04-08 20:41:55 +0800

    feat: 优化社区转发栏目的顺序

--- src/pages/components/_CommunityArticle/data.ts
+++ src/pages/components/_CommunityArticle/data.ts
@@ -7,16 +7,10 @@ interface ListItem {
 
 export const listData: ListItem[] = [
   {
-    url: 'https://mp.weixin.qq.com/s/x75dqt6FDHk5OEv13tr_tw',
-    title: '前端定时任务的神库 Cron！，快把它加到你的项目中去！',
-    createDate: '2024-03-30',
-    from: '微信公众号'
-  },
-  {
-    url: 'https://mp.weixin.qq.com/s/24RhBwvDyS4GVWhwWVHgkQ',
-    title: '前端神库 Puppeteer ！让你能在 Nodejs 里操作浏览器玩出花样！',
-    createDate: '2024-04-07',
-    from: '微信公众号'
+    url: 'https://juejin.cn/post/6844903926110617613',
+    title: '灵活运用CSS开发技巧，布局、行为、色彩、图形、组件等',
+    createDate: '2024-04-08',
+    from: '掘金'
   },
   {
     url: 'https://segmentfault.com/a/1190000039311939',
@@ -25,9 +19,15 @@ export const listData: ListItem[] = [
     from: '思否'
   },
   {
-    url: 'https://juejin.cn/post/6844903926110617613',
-    title: '灵活运用CSS开发技巧，布局、行为、色彩、图形、组件等',
-    createDate: '2024-04-08',
-    from: '掘金'
+    url: 'https://mp.weixin.qq.com/s/24RhBwvDyS4GVWhwWVHgkQ',
+    title: '前端神库 Puppeteer ！让你能在 Nodejs 里操作浏览器玩出花样！',
+    createDate: '2024-04-07',
+    from: '微信公众号'
+  },
+  {
+    url: 'https://mp.weixin.qq.com/s/x75dqt6FDHk5OEv13tr_tw',
+    title: '前端定时任务的神库 Cron！，快把它加到你的项目中去！',
+    createDate: '2024-03-30',
+    from: '微信公众号'
   },
 ]
