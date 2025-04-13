327ea879956b3bb0d0d5470a947234ee5e1e4536
CoderBin <1669940775@qq.com>
2025-04-13 12:30:59 +0800

    feat: 开发站点导航基础页面

---
+++ src/pages/components/_SiteNavigation/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './styles.module.scss';
+
+/**
+ * 站点导航
+ * @returns 
+ */
+export default function SiteNavigation() {
+  return (
+    <div className={styles.SiteNavigation}>
+      开发中。。。
+    </div>
+  )
+}
