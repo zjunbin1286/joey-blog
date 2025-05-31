28f564ec7e2ff326a2c8c81fdd57daabd8811a99
CoderBin <1669940775@qq.com>
2025-05-31 22:36:18 +0800

    feat: 技术社区文章增加《等等，fetch 请求还能被取消？》

--- src/pages/components/_CommunityArticle/data.ts
+++ src/pages/components/_CommunityArticle/data.ts
@@ -6,64 +6,70 @@ interface ListItem {
 }
 
 export const listData: ListItem[] = [
-    {
+  {
+    url: 'https://juejin.cn/post/7026941253845516324',
+    title: '等等，fetch 请求还能被取消？',
+    createDate: '2025-05-31',
+    from: '掘金'
+  },
+  {
     url: 'https://juejin.cn/post/7026941253845516324',
     title: 'Fabric.js 从入门到________',
-    createDate: '2024-05-26',
+    createDate: '2025-05-26',
     from: '掘金'
   },
   {
     url: 'https://mp.weixin.qq.com/s/yfhaqyVd7up_lWyJ6xqPgA',
     title: '前端页面中，如何让用户回到上次阅读的位置？',
-    createDate: '2024-04-18',
+    createDate: '2025-04-18',
     from: '微信公众号'
   },
   {
     url: 'https://juejin.cn/post/7023906112843808804',
     title: '史上最全！熬夜整理56个JavaScript高级的手写知识点！！专业扫盲！',
-    createDate: '2024-04-18',
+    createDate: '2025-04-18',
     from: '掘金'
   },
   {
     url: 'https://mp.weixin.qq.com/s/zpH0vuz-Q37qK8Qc0NgmAA',
     title: '前端文件操作黑魔法！Blob和File让你玩转文件流',
-    createDate: '2024-04-14',
+    createDate: '2025-04-14',
     from: '微信公众号'
   },
   {
     url: 'https://developer.chrome.google.cn/docs/devtools/performance?hl=zh-cn',
     title: '如何使用 Chrome DevTools 中的“性能”面板分析运行时性能，分析网页的响应、动画和空闲阶段',
-    createDate: '2024-04-14',
+    createDate: '2025-04-14',
     from: '谷歌开发者文章'
   },
   {
     url: 'https://mp.weixin.qq.com/s/AZV7rJWO1pZOQ2M6136EUg',
     title: '2025年十大CSS新特性：已在所有主流浏览器中支持',
-    createDate: '2024-04-12',
+    createDate: '2025-04-12',
     from: '微信公众号'
   },
   {
     url: 'https://juejin.cn/post/6844903926110617613',
     title: '灵活运用CSS开发技巧，布局、行为、色彩、图形、组件等',
-    createDate: '2024-04-08',
+    createDate: '2025-04-08',
     from: '掘金'
   },
   {
     url: 'https://segmentfault.com/a/1190000039311939',
     title: '中高级前端必须注意的40条移动端H5坑位指南 | 网易三年实践',
-    createDate: '2024-04-07',
+    createDate: '2025-04-07',
     from: '思否'
   },
   {
     url: 'https://mp.weixin.qq.com/s/24RhBwvDyS4GVWhwWVHgkQ',
     title: '前端神库 Puppeteer ！让你能在 Nodejs 里操作浏览器玩出花样！',
-    createDate: '2024-04-07',
+    createDate: '2025-04-07',
     from: '微信公众号'
   },
   {
     url: 'https://mp.weixin.qq.com/s/x75dqt6FDHk5OEv13tr_tw',
     title: '前端定时任务的神库 Cron！，快把它加到你的项目中去！',
-    createDate: '2024-03-30',
+    createDate: '2025-03-30',
     from: '微信公众号'
   },
 ]
