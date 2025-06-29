41c8d7fbc9038af111989c2871b1dbf6a4582ad2
CoderBin <1669940775@qq.com>
2025-06-29 23:05:29 +0800

    feat: 修改我的页面

--- src/pages/components/_AboutMe/index.tsx
+++ src/pages/components/_AboutMe/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function AboutMe() {
   return (
     <div>
-      <h2>Hey, I' Joey~</h2>
+      <h2>Hey, I' Joey</h2>
       {/* <a href="https://juejin.cn/user/1627704066072712">掘金：CoderBin逆玄</a><br/>
       <a href="https://juejin.cn/user/1627704066072712">掘金：CoderBin逆玄</a> */}
     </div>
